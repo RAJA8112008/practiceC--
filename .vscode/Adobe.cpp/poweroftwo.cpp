014912fb008f4b9df8787f826d3cebf5277cb876
Raja <kraj9380286@gmail.com>
2026-01-14 19:14:22 +0530

    commited

--- .vscode/Adobe.cpp/poweroftwo.cpp
+++ .vscode/Adobe.cpp/poweroftwo.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 using namespace std;
 bool ispower(int n){
-    if(n<=0)return false;
-    return n&(n-1);
+    if(n<=0)return 0;
+    return (n&(n-1))==0;
 }
 int main(){
     int n;
